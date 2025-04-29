69ff15d40f18427d0944ecddf62d510ee0986b3e
Aditya Gangwar <manojkumar1826@rediffmail.com>
2025-04-29 23:12:15 +0530

    final

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    MONGO_URI: process.env.MONGO_URI,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
