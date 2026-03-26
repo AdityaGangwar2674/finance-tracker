4bcfad3cf255eb5cc44b387e9aec116d99f75b4d
Aditya Gangwar <manojkumar1826@rediffmail.com>
2026-03-26 13:54:29 +0530

    [chore]: deployment fixes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    MONGO_URI: process.env.MONGO_URI,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
